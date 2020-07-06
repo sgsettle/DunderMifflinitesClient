6ea2aab4710ddb3a32736cc2daa91cb372fa2c53
KateLockhart <katelockhart707@gmail.com>
2020-07-06 11:29:46 -0400

    initial kate commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Develop Branch</h1>
-      <h2>Slayde's Branch</h2>
-      <h3>Brad's branch</h3>
+      
     </div>
   );
 }
